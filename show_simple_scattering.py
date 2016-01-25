0010c55673ab7277ac285a84ae89c05699502bd9
doksa <ivan.dokmanic@epfl.ch>
2016-01-25 16:16:38 +0100

    Various bug fixes in the filters. Implemented multiresolution filters. Implemented the full 2D scattering transform. Changed the transform indexing scheme to tuples of tuples. Added support for Intel's MKL FFT (fast). ... and much more.

--- show_simple_scattering.py
+++ show_simple_scattering.py
@@ -3,10 +3,12 @@ from scatterpyllar.core.scattering_simple import scattering_layer_0
 import numpy as np
 from scipy.misc import lena
 
+
 if __name__ == "__main__":
     lena = lena() / 256.
+    lena = lena[::4,::4]
 
-    filters = morlet_filter_bank_2d(lena.shape, L=4, sigma_phi=16,
+    filters = morlet_filter_bank_2d(lena.shape, J=7, L=4, sigma_phi=16,
                                     sigma_psi=16,
                                     xi_psi=np.pi / 64.)
 
