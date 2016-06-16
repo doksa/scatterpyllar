48d6ce89df2ade3312d5815af66c88fb38b3de7c
doksa <ivan.dokmanic@epfl.ch>
2016-06-16 14:51:31 +0200

    Added examples of addressing coeffs in first and zero-th layers.

--- test_scattering.py
+++ test_scattering.py
@@ -38,14 +38,24 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 
 # This is how you access coefficients in the second layer:
-
 print S['coeffs'][((0, 0),(3, 1),(5, 1))]
 
-# This indexes a path through layers: 
 
+# So indexing is done by tuples of (j, l) tuples. The one above indexes the
+# following path through layers:
+#
 # Layer 0: (0, 0) (this is always the same) 
 # Layer 1: (3, 1) meaning j = 3 (note that j starts from 1!), and l = 1;
-# unlike with j, l = 1 indexes the second rotation (first one you get for l = 0)
+#                 unlike with j, l = 1 indexes the second rotation (first 
+#                 one you get for l = 0)
 # Layer 2: (5, 1) analogously
+
+
+# This is how you access coefficients in the first layer:
+print S['coeffs'][((0, 0),(3, 1))]
+
+# This is how you access the mean coeff in the zero-th layer (note the comma!):
+print S['coeffs'][((0, 0),)]
+
 
 
